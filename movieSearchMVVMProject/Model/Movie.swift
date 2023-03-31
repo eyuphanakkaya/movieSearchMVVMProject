5f8d0dc9dbb144a8892acc563422cd603e48d127
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2023-03-31 15:52:26 +0300

    Update

---
+++ movieSearchMVVMProject/Model/Movie.swift
@@ -0,0 +1,32 @@
+//
+//  Movie.swift
+//  movieSearchMVVMProject
+//
+//  Created by Eyüphan Akkaya on 31.03.2023.
+//
+
+import Foundation
+
+struct Movie : Codable {
+    let title:String
+    let year:String
+    let imdbId:String
+    let type:String
+    let poster:String
+    
+    private enum CodingKeys: String,CodingKey {//burada proplarımızın bazen metot olabileceği için internetten çektiğimiz verileri enum yapısıyla karşılığını giriyoruz
+        case title = "Title"
+        case year = "Year"
+        case imdbId = "imbdId"
+        case type = "Type"
+        case poster = "Poster"
+    }
+    
+}
+struct IncomingData : Codable {
+    let movies: [Movie]
+    
+    private enum CodingKeys : String,CodingKey {
+        case movies = "Search "
+    }
+}
---
+++ movieSearchMVVMProject/Service/DownLoaderClient.swift
@@ -0,0 +1,16 @@
+//
+//  DownLoaderClient.swift
+//  movieSearchMVVMProject
+//
+//  Created by Eyüphan Akkaya on 31.03.2023.
+//
+
+import Foundation
+
+
+class DownLoaderClient {
+    func downloadMovies() {
+        
+    }
+}
+
