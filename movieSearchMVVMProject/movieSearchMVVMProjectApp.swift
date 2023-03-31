98418a8ec050377d0b97de83a90efd798a7783a0
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2023-03-31 14:31:05 +0300

    Initial Commit

---
+++ movieSearchMVVMProject/movieSearchMVVMProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieSearchMVVMProjectApp.swift
+//  movieSearchMVVMProject
+//
+//  Created by Eyüphan Akkaya on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct movieSearchMVVMProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
